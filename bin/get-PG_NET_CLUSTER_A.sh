6db61dc9ed4e932d58e979cbc3716d37de8f2203
tiredpixel <tiredpixel@posteo.de>
2017-02-25 12:59:17 +0000

    [#10] bin: remove sudo from helper script

--- bin/get-PG_NET_CLUSTER_A.sh
+++ bin/get-PG_NET_CLUSTER_A.sh
@@ -2,6 +2,6 @@
 
 PG_NET_CLUSTER_N=${1:-'postgresxldocker_postgres-a'}
 
-PG_NET_CLUSTER_A=$(sudo docker network inspect -f '{{range .IPAM.Config}}{{.Subnet}}{{end}}' $PG_NET_CLUSTER_N | tr -d '\n')
+PG_NET_CLUSTER_A=$(docker network inspect -f '{{range .IPAM.Config}}{{.Subnet}}{{end}}' $PG_NET_CLUSTER_N | tr -d '\n')
 
 echo $PG_NET_CLUSTER_A
