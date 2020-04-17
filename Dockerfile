68a47ddaa632892e535ac71a4b0403df80222935
tiredpixel <tiredpixel@posteo.de>
2020-04-17 16:17:39 +0000

    [#1662] upgrade

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 #===============================================================================
-FROM debian:9
+# FROMFREEZE docker.io/library/debian:9
+FROM docker.io/library/debian@sha256:d844caef45253dab4cb7543b5781f529c1c3f140fcf9cd6172e1d6cb616a51c3
 
 ARG PG_HOME=/var/lib/postgresql
 ARG PG_LIB=/usr/local/lib/postgresql
--- ci/meta.sh
+++ ci/meta.sh
@@ -1,9 +1,36 @@
-#!/bin/sh -e
+#!/bin/bash -eu
+set -o pipefail
 
 src=$PWD/src
 meta=$PWD/meta
 
-cd "$src"
-git describe | tr -d '\n' > "$meta/version"
+branch_latest=master
 
-cat "$meta"/*
+test -d "$meta" || meta=$(mktemp -d)
+#-------------------------------------------------------------------------------
+cd "$src"
+
+version=$(git describe)
+
+echo -n "$version" > "$meta/version"
+touch "$meta/tags"
+
+release=$(git describe --abbrev=0)
+
+if [ "$version" == "$release" ]; then
+    release_1=$(echo "$release" | cut -d'.' -f1)
+    release_2=$(echo "$release" | cut -d'.' -f2)
+    
+    # release tagged for completeness, but version same
+    echo -n "$release $release_1.$release_2 $release_1 " >> "$meta/tags"
+fi
+
+commit_latest=$(git rev-parse "$branch_latest")
+commit_head=$(git rev-parse HEAD)
+
+if [ "$commit_head" == "$commit_latest" ]; then
+    echo -n "latest " >> "$meta/tags"
+fi
+#-------------------------------------------------------------------------------
+echo "version: $(cat "$meta/version")"
+echo "tags:    $(cat "$meta/tags")"
